68731d588f7efe86717532e29bb62fdf4d064077
iampartho <46246594+iampartho@users.noreply.github.com>
2020-08-30 21:33:39 +0600

    Return to default custom.py

--- mmdet/datasets/custom.py
+++ mmdet/datasets/custom.py
@@ -2,13 +2,17 @@ import os.path as osp
 
 import mmcv
 import numpy as np
+import random
+import torch
 from torch.utils.data import Dataset
 import torchvision.transforms.functional as tvf
+import torch.nn.functional as tnf
+import scipy.ndimage
 
 from mmdet.core import eval_map, eval_recalls
 from .builder import DATASETS
 from .pipelines import Compose
-import extra_augmentation as augUtils
+
 
 @DATASETS.register_module()
 class CustomDataset(Dataset):
@@ -111,43 +115,6 @@ class CustomDataset(Dataset):
 
 
 
-
-    def augment_PIL(self, img):
-        if np.random.rand() > 0.4:
-            img = tvf.adjust_brightness(img, uniform(0.3,1.5))
-        if np.random.rand() > 0.5:
-            factor = 2 ** uniform(-1, 1)
-            img = tvf.adjust_contrast(img, factor) # 0.5 ~ 2
-        if np.random.rand() > 0.6:
-            img = tvf.adjust_hue(img, uniform(-0.1,0.1))
-        if np.random.rand() > 0.6:
-            factor = uniform(0,2)
-            if factor > 1:
-                factor = 1 + uniform(0, 2)
-            img = tvf.adjust_saturation(img, factor) # 0 ~ 3
-        if np.random.rand() > 0.5:
-            img = tvf.adjust_gamma(img, uniform(0.5, 3))
-
-
-        if np.random.rand() > 0.5:
-            img = augUtils.add_gaussian(img, max_var=0.03)
-        blur = [augUtils.random_avg_filter, augUtils.max_filter,
-                augUtils.random_gaussian_filter]
-        if np.random.rand() > 0.8:
-            blur_func = random.choice(blur)
-            img = blur_func(img)
-        if np.random.rand() > 0.5:
-            img = augUtils.add_saltpepper(img, max_p=0.04)
-        # horizontal flip
-        
-        # # random rotation
-        # rand_degree = np.random.rand() * 360
-        # if self.coco:
-        #     img, labels = augUtils.rotate(img, rand_degree, labels, expand=True)
-        # else:
-        #     img, labels = augUtils.rotate(img, rand_degree, labels, expand=False)
-        return img
-
     def load_proposals(self, proposal_file):
         """Load proposal from proposal file."""
         return mmcv.load(proposal_file)
@@ -228,7 +195,8 @@ class CustomDataset(Dataset):
             if data is None:
                 idx = self._rand_another(idx)
                 continue
-            print("\n\n\n\nThis is the data keys and type\n", data.keys(), type(data))
+            #print("\n\n\n\nThis is the image \n", data['img'])
+            #data['img'] = self.augment_PIL(data['img'])
             return data
 
     def prepare_train_img(self, idx):
@@ -248,6 +216,7 @@ class CustomDataset(Dataset):
         if self.proposals is not None:
             results['proposals'] = self.proposals[idx]
         self.pre_pipeline(results)
+        #print("\n\n\n\nThe result dictionary keys \n", results.keys(), type(results))
         return self.pipeline(results)
 
     def prepare_test_img(self, idx):
